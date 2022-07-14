59e7b304e57b63986c40b260c68c1f371d9d293c
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-07-14 18:09:30 -0300

    Update FormatarInput.js

--- Aulas/Desafios/Desafio02/scripts/FormatarInput.js
+++ Aulas/Desafios/Desafio02/scripts/FormatarInput.js
@@ -1,7 +1,8 @@
 function FormatarInput() {
     // Adicionar os valores máximos para o input[type="date"] (dia atual)
     var data = new Date
-    var dataAtual = `${data.getFullYear()}-${data.getMonth() + 1}-${data.getDate()}`  // yy-mm-dd
+    var dataAtual = `${data.getFullYear()}-${data.getMonth() + 1}-${data.getDate()}`  // yyyy-mm-dd
     var input = document.querySelector('#dataNascimento')
+
     input.setAttribute('max', dataAtual)
 }
