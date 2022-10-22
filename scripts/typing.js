7b75c20ddd072391fa24cae625c5fba69f8b9c68
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-10-22 17:26:01 -0300

    Update typing.js

--- scripts/typing.js
+++ scripts/typing.js
@@ -4,18 +4,17 @@ function typingVar() {
     element.style.color = 'black';
     string = element.innerHTML.split('');
     element.innerHTML = ''
-    var timer;
     typingText()
 }
 
 function typingText() {
     if (string.length > 0) {
         element.innerHTML += string.shift();
-        LoopTimer = setTimeout('typingText()', 100);
+        setTimeout('typingText()', 100);
     } else {
         element.innerHTML += '_'
         document.body.style.color = 'black'
         clearTimeout(0);
-        setTimeout(a = () => {}, 100)
+        setTimeout(a = () => {}, 300)
     }
 }
