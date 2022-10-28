6d3362e9829be0bc9e68f62bf4f56944d840586d
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-10-28 08:49:24 -0300

    Delete files

--- Exercicios/ex014/mostrarHora.js
+++
@@ -1,6 +0,0 @@
-function mostrarHora() {
-	var hora = Date()
-	var resultado = document.getElementById('resultado')
-
-	resultado.innerHTML = `<p>O que eu recebi do sistema foi <mark>${hora}</mark></p>`
-}	
