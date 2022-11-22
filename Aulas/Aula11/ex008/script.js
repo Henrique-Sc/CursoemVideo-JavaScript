cba2f0bd1f25be0ab5aa1ccbf795c37ac85103bf
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-11-22 12:46:50 -0300

    Create script.js

---
+++ Aulas/Aula11/ex008/script.js
@@ -0,0 +1,9 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+
+if (vel > 60) {
+    console.log('Você ultraprassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança.')
