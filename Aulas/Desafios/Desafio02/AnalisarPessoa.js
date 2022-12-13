4f49565a82e888d214ecf923891d55d046fdab6d
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-12-13 17:44:52 -0300

    Update AnalisarPessoa.js

--- Aulas/Desafios/Desafio02/AnalisarPessoa.js
+++ Aulas/Desafios/Desafio02/AnalisarPessoa.js
@@ -1,6 +1,25 @@
 function analisar() {
-    // Variáveis de input
+    var data = new Date()
+    var sexo
+
+    var anoNascimento = Number(document.querySelector('#anoNascimento').value)
+    var sexoRadio = document.getElementsByName('sexo')
+    sexoRadio.forEach(radio => {
+        if (radio.checked) {
+            sexo = radio.value
+        } else {
+            sexo = false
+        }
+    })
 
     // Variáveis de output
-    
+    var result_desc = document.querySelector('#result_desc')
+    var result_img = document.querySelector('#result_img')
+
+    // Verificar se os inputs estão vazios
+    if (anoNascimento < 1800 || sexo == false) {
+        alert('Valores inválidos!')
+    }
+
+    idade = data.getFullYear() - anoNascimento
 }
