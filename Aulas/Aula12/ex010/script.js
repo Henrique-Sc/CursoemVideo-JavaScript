f714b42e0ac0368ace36ed12c7d1e9ebad273605
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-12-19 11:11:41 -0300

    Update script.js

--- Aulas/Aula12/ex010/script.js
+++ Aulas/Aula12/ex010/script.js
@@ -1,3 +1,15 @@
-function a() {
+function eleitor() {
     var result = document.querySelector('#result')
+    var idade = Number(document.querySelector('#idade').value)
+
+    var result_msg = ''
+    if (idade < 16) {
+        result_msg = 'você não tem a idade mínima para votar (16 anos)'
+    } else if (idade < 18 || idade >= 70) {
+        result_msg = 'o seu voto é facultivo'
+    } else {
+        result_msg = 'o seu voto é obrigatório'
+    }
+
+    result.innerHTML = `<p>TSE informa: ${result_msg}.</p>`
 }
