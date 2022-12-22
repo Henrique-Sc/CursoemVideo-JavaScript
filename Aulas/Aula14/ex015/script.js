6ebee768a572958f6ac28334f3f70e1d8a5e64b5
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-12-22 14:16:54 -0300

    Comentando o código

--- Aulas/Aula14/ex015/script.js
+++ Aulas/Aula14/ex015/script.js
@@ -12,13 +12,17 @@ function tabuada() {
     // Variável de output
     var result = document.querySelector('#result')
 
+    // Verificando os inputs
     if (f < i) {
         result.innerHTML = '<p>Impossível gerar a tabuada, fim maior que início.</p>'
     } else {
+        // Avisando o usuário que pode demorar parar para processar os dados
         if (f - i >= 1000) {
             alert('O programa terá que processar muitos dados, aguarde por favor.')
         }
-        result.innerHTML = ''  // "Limpando" a saída dos dados
+
+        // "Limpando" a saída dos dados
+        result.innerHTML = '' 
 
         // Gerando a tabuada
         while (i <= fim) {
@@ -26,11 +30,13 @@ function tabuada() {
             i++
         }
 
+        // Colocando a visibilidade do btnLimpar para visível
         document.querySelector('#btnLimpar').style.visibility = 'visible'
     }
 }
 
 function limpar(btn) {
+    // Reseta o result e oculpa o botão
     document.querySelector('#result').innerHTML = 'O resultado aparecerá aqui...'
     btn.style.visibility = 'hidden'
 }
