dfdbeb9df8aa186efe7f0103df5aab807720f1a7
Henrique da Silva Costa <henriquesc1442@gmail.com>
2023-01-04 14:47:21 -0300

    Update script.js

--- Aulas/Aula16/ex018/script.js
+++ Aulas/Aula16/ex018/script.js
@@ -8,48 +8,87 @@ function adicionar() {
 
     // Verificando o input
     if (num.length <= 0) {
-        result.innerHTML = '<p><span style="color:red">ERRO:</span> Digite um valor.</p>'
+        escreverResult(error('Digite um valor.'))        
     } else if (num < 1) {
-        result.innerHTML = '<p><span style="color:red">ERRO:</span> Digite um valor maior ou igual a 1.</p>'
+        escreverResult(error('Digite um valor maior ou igual a 1.'))
     } else if (num > 100) {
-        result.innerHTML = '<p><span style="color:red">ERRO:</span> Digite um valor menor ou igual a 100.</p>'
+        escreverResult(error('Digte um valor menor ou igual a 100.'))
     } else {        
-        result.innerHTML = '<p>O resultado aparecerá aqui...</p>'
+        escreverResult('O resultado aparecerá aqui')
 
         // Está da forma padrão, sem nehum número
         if (DivNumeros.innerHTML == '<p>Insira</p><p>Um</p><p>Número</p>') {
             DivNumeros.innerHTML = ''
+
+            // Ativar o botão Finalizar
+            let btnFinalizar = document.querySelector('#btnFinalizar')
+            btnFinalizar.disabled = false
+            btnFinalizar.style.cursor = 'pointer'
         }
 
-        var arr = Array.prototype.slice.call(DivNumeros.children)
-        lo
-        console.log(DivNumeros.children);
-        // Adicionar o número na div
-        DivNumeros.innerHTML += `<p>${num}</p>`
+        // Pegar os children na DivNumeros
+        var children = Array.prototype.slice.call(DivNumeros.children)
 
-        // Ativar o botão Finalizar
-        let btnFinalizar = document.querySelector('#btnFinalizar')
-        btnFinalizar.disabled = false
-        btnFinalizar.style.cursor = 'pointer'
+        // Analisar os valores
+        let podeAdicionar = true
+        children.forEach(e => {
+            if (e.innerHTML == num) {
+                // O número digitado já existe na Div
+                result.innerHTML = '<p>Número já adicionado. Insira outro valor!</p>'
+                podeAdicionar = false
+            }
+        })
+
+        if (podeAdicionar) {
+            DivNumeros.innerHTML += `<p>${num}</p>`
+        }
 
     }
 }
+
 
 function finalizar(btn) {
     // Variável de output
     let result = document.querySelector('#result')
     let DivNumeros = document.querySelector('#numeros')
 
-    /*
-        -> Só pode ser pressionado se 
-    */
-    btn.disabled = true
-    btn.style.cursor = 'no-drop'
-    // Colocando a visibilidade do btnLimpar para visível
-    document.querySelector('#btnResetar').style.visibility = 'visible'
-    
+    if (DivNumeros.innerHTML == '<p>Insira</p><p>Um</p><p>Número</p>') {
+        result.innerHTML = error('Insira pelo menos um número primeiro!')
+
+    } else {
+
+        escreverResult('', true)
+
+        var children = Array.prototype.slice.call(DivNumeros.children)
+        
+        // Analisar os números
+        children.forEach(e => {
+            result.innerHTML += `${e.innerHTML}`
+        })
+
+        btn.disabled = true
+        btn.style.cursor = 'no-drop'
+
+        // Colocando a visibilidade do btnLimpar para visível
+        document.querySelector('#btnResetar').style.visibility = 'visible'
+    }
 }
+
 
 function resetar() {
     location.reload()
+}
+
+
+function error(msg) {
+    return `<p><span style="color:red">ERRO:</span> ${msg}</p>`
+}
+
+
+function escreverResult(msg, sobreescrever=true) {
+    if (sobreescrever) {
+        document.querySelector('#result').innerHTML = msg
+    } else {
+        document.querySelector('#result').innerHTML += msg
+    }
 }
